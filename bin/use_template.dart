afc37f5711de216b076be3925b0cfad45aefa09b
baranacikgoz <baran-acikgoz@outlook.com>
2022-05-26 23:24:31 +0300

    initial

---
+++ bin/use_template.dart
@@ -0,0 +1,5 @@
+import 'package:use_template/use_template.dart' as use_template;
+
+void main(List<String> arguments) {
+  print('Hello world: ${use_template.calculate()}!');
+}
---
+++ test/use_template_test.dart
@@ -0,0 +1,8 @@
+import 'package:use_template/use_template.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
