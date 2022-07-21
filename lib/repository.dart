f473fb87e21078af67c4b17d6929dc2ccc346e6d
baranacikgoz <baran-acikgoz@outlook.com>
2022-07-21 14:21:50 +0300

    implemented 'changelinuxName'

--- lib/repository.dart
+++ lib/repository.dart
@@ -60,6 +60,14 @@ class Repository {
       newNameSnakeCase: newAppNameSnakeCase,
       newNameUpperedFirstChars: newNameUpperedFirstChars,
     );
+
+    // Change linux name.
+    _changeLinuxName(
+      path: pathToInstall,
+      oldName: _oldName,
+      newNameSnakeCase: newAppNameSnakeCase,
+      newNameUpperedFirstChars: newNameUpperedFirstChars,
+    );
   }
 
   void _createDirectory(String path) {
@@ -171,5 +179,36 @@ class Repository {
       }
     }
     infoPlistFile.writeAsStringSync(lines.join('\n'));
+  }
+
+  void _changeLinuxName({
+    required String path,
+    required String oldName,
+    required String newNameSnakeCase,
+    required String newNameUpperedFirstChars,
+  }) {
+    // Check if Linux path exists.
+    if (!Directory(join(path, 'linux')).existsSync()) {
+      printerr(
+        "Couldn't found Linux directory, probably your app doesn't have a Linux project.",
+      );
+      return;
+    }
+
+    // Read CMakeLists.txt and replace name.
+    final cmakeListsFile = File(join(path, 'linux', 'CMakeLists.txt'));
+    final cmakeListsFileContent = cmakeListsFile.readAsStringSync();
+
+    cmakeListsFile.writeAsStringSync(
+      cmakeListsFileContent.replaceAll(oldName, newNameSnakeCase),
+    );
+
+    // Read my_application.cc and replace name.
+    final myApplicationFile = File(join(path, 'linux', 'my_application.cc'));
+    final myApplicationFileContent = myApplicationFile.readAsStringSync();
+
+    myApplicationFile.writeAsStringSync(
+      myApplicationFileContent.replaceAll(oldName, newNameUpperedFirstChars),
+    );
   }
 }
