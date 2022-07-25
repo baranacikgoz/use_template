f4fbcae7c0b8b639cf419bf84c016017570cd30d
baranacikgoz <baran-acikgoz@outlook.com>
2022-07-25 14:36:52 +0300

    added 'changeMacOSName'

--- lib/repository.dart
+++ lib/repository.dart
@@ -170,10 +170,6 @@ class Repository {
         lines[i + 1] = '<string>$newNameUpperedFirstChars</string>';
       }
 
-      if (lines[i].contains('CFBundleDisplayName')) {
-        lines[i + 1] = '<string>$newNameUpperedFirstChars</string>';
-      }
-
       if (lines[i].contains('CFBundleName')) {
         lines[i + 1] = '<string>$newNameSnakeCase</string>';
       }
@@ -210,5 +206,34 @@ class Repository {
     myApplicationFile.writeAsStringSync(
       myApplicationFileContent.replaceAll(oldName, newNameUpperedFirstChars),
     );
+  }
+
+  void _changeMacOSName({
+    required String path,
+    required String oldName,
+    required String newNameSnakeCase,
+    required String newNameUpperedFirstChars,
+  }) {
+    // Check if MacOS path exists.
+    if (!Directory(join(path, 'macos')).existsSync()) {
+      printerr(
+        "Couldn't found MacOS directory, probably your app doesn't have a MacOS project.",
+      );
+      return;
+    }
+
+    // Read Info.plist and replace name.
+    final infoPlistFile = File(join(path, 'macos', 'Info.plist'));
+    final List<String> lines = infoPlistFile.readAsLinesSync();
+    for (int i = 0; i < lines.length; i++) {
+      if (lines[i].contains('CFBundleDisplayName')) {
+        lines[i + 1] = '<string>$newNameUpperedFirstChars</string>';
+      }
+
+      if (lines[i].contains('CFBundleName')) {
+        lines[i + 1] = '<string>$newNameSnakeCase</string>';
+      }
+    }
+    infoPlistFile.writeAsStringSync(lines.join('\n'));
   }
 }
