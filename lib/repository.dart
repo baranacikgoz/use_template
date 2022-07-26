6038295d988b355ae6f9a0d1566947ef52688e15
baranacikgoz <baran-acikgoz@outlook.com>
2022-07-26 13:05:16 +0300

    implemented 'changeWindowsName'

--- lib/repository.dart
+++ lib/repository.dart
@@ -69,6 +69,14 @@ class Repository {
       newNameUpperedFirstChars: newNameUpperedFirstChars,
     );
 
+    // Change windows name.
+    _changeWindowsName(
+      path: pathToInstall,
+      oldName: _oldName,
+      newNameSnakeCase: newAppNameSnakeCase,
+      newNameUpperedFirstChars: newNameUpperedFirstChars,
+    );
+
     // Change macos name.
     _changeMacOSName(
       path: pathToInstall,
@@ -183,6 +191,45 @@ class Repository {
       }
     }
     infoPlistFile.writeAsStringSync(lines.join('\n'));
+  }
+
+  void _changeWindowsName({
+    required String path,
+    required String oldName,
+    required String newNameSnakeCase,
+    required String newNameUpperedFirstChars,
+  }) {
+    // Check if Windows path exists.
+    if (!Directory(join(path, 'windows')).existsSync()) {
+      printerr(
+        "Couldn't found Windows directory, probably your app doesn't have an Windows project.",
+      );
+      return;
+    }
+
+    // Read CMakeLists.txt and replace name.
+    final cmakeListsFile = File(join(path, 'windows', 'CMakeLists.txt'));
+    final cmakeListsFileContent = cmakeListsFile.readAsStringSync();
+
+    cmakeListsFile.writeAsStringSync(
+      cmakeListsFileContent.replaceAll(oldName, newNameSnakeCase),
+    );
+
+    // Read my_application.cc and replace name.
+    final myApplicationFile = File(join(path, 'windows', 'runner', 'main.cpp'));
+    final myApplicationFileContent = myApplicationFile.readAsStringSync();
+
+    myApplicationFile.writeAsStringSync(
+      myApplicationFileContent.replaceAll(oldName, newNameUpperedFirstChars),
+    );
+
+    // Read Runner.rc and replace name.
+    final runnerRcFile = File(join(path, 'windows', 'runner', 'Runner.rc'));
+    final runnerRcFileContent = runnerRcFile.readAsStringSync();
+
+    runnerRcFile.writeAsStringSync(
+      runnerRcFileContent.replaceAll(oldName, newNameSnakeCase),
+    );
   }
 
   void _changeLinuxName({
