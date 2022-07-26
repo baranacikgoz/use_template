2f15e3711fbbe8cbcfc93a3a0ebccfbad4b98af1
baranacikgoz <baran-acikgoz@outlook.com>
2022-07-26 13:12:59 +0300

    implemented 'changeWebName'

--- lib/repository.dart
+++ lib/repository.dart
@@ -61,6 +61,14 @@ class Repository {
       newNameUpperedFirstChars: newNameUpperedFirstChars,
     );
 
+    // Change Web name.
+    _changeWebName(
+      path: pathToInstall,
+      oldName: _oldName,
+      newNameSnakeCase: newAppNameSnakeCase,
+      newNameUpperedFirstChars: newNameUpperedFirstChars,
+    );
+
     // Change linux name.
     _changeLinuxName(
       path: pathToInstall,
@@ -191,6 +199,35 @@ class Repository {
       }
     }
     infoPlistFile.writeAsStringSync(lines.join('\n'));
+  }
+
+  void _changeWebName({
+    required String path,
+    required String oldName,
+    required String newNameSnakeCase,
+    required String newNameUpperedFirstChars,
+  }) {
+    // Check if web path exists.
+    if (!Directory(join(path, 'web')).existsSync()) {
+      printerr(
+        "Couldn't found web directory, probably your app doesn't have a web project.",
+      );
+      return;
+    }
+
+    final webIndexFile = File(join(path, 'web', 'index.html'));
+    final webIndexContent = webIndexFile.readAsStringSync();
+
+    webIndexFile.writeAsStringSync(
+      webIndexContent.replaceAll(oldName, newNameUpperedFirstChars),
+    );
+
+    final webManifestFile = File(join(path, 'web', 'manifest.json'));
+    final webManifestContent = webManifestFile.readAsStringSync();
+
+    webManifestFile.writeAsStringSync(
+      webManifestContent.replaceAll(oldName, newNameSnakeCase),
+    );
   }
 
   void _changeWindowsName({
