6d094ad3ad9a54615abb29d6d1a7b189d4d0ed64
baranacikgoz <baran-acikgoz@outlook.com>
2022-07-27 16:18:51 +0300

    implemented 'changePubspecName' and 'changeLibImports'

--- lib/src/name_changer_methods/change_flutter_name.dart
+++ lib/src/name_changer_methods/change_flutter_name.dart
@@ -2,16 +2,37 @@ import 'dart:io';
 
 import 'package:dcli/dcli.dart';
 
-void changePubspecName(
-  String baseFolderPath,
-  String oldName,
-  String newNameSnakeCase,
-  String newNameUpperedFirstChars,
-) {
+/// Changes the name of the new clonned Flutter package / project from pubspec.
+void changePubspecName({
+  required String baseFolderPath,
+  required String oldName,
+  required String newNameSnakeCase,
+  required String newNameUpperedFirstChars,
+}) {
   final pubspecFile = File(join(baseFolderPath, 'pubspec.yaml'));
   final pubspecContent = pubspecFile.readAsStringSync();
 
   pubspecFile.writeAsStringSync(
-    pubspecContent.replaceFirst(oldName, newNameSnakeCase),
+    pubspecContent.replaceAll(oldName, newNameSnakeCase),
   );
+}
+
+/// Changes the all import names under lib folder from old project name to new project name.
+void changeLibImportNames({
+  required String baseFolderPath,
+  required String oldName,
+  required String newNameSnakeCase,
+  required String newNameUpperedFirstChars,
+}) {
+  final libFolder = Directory(join(baseFolderPath, 'lib'));
+  final libFiles = libFolder.listSync();
+
+  for (final file in libFiles) {
+    if (file is File) {
+      final fileContent = file.readAsStringSync();
+      file.writeAsStringSync(
+        fileContent.replaceAll(oldName, newNameSnakeCase),
+      );
+    }
+  }
 }
